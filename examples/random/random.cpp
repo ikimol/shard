c6ca3716040783b73e647f56b8372982887cc318
Miklos Molnar <miklos.molnar@me.com>
2021-03-02 19:59:18 +0100

    Create 'random' module

---
+++ examples/random/random.cpp
@@ -0,0 +1,43 @@
+// Copyright (c) 2021 Miklos Molnar. All rights reserved.
+
+#include <shard/random.hpp>
+
+#include <ctime>
+#include <iostream>
+#include <map>
+
+int main(int argc, char* argv[]) {
+    shard::random<int> r_int(0, 10);
+    std::cout << r_int.next() << '\n';
+
+    shard::random<float> r_float(0.5f, 1.5f);
+    std::cout << r_float.next() << '\n';
+
+    // query the seeds of the generators
+    std::cout << "r_int.seed = " << r_int.seed() << '\n';
+    std::cout << "r_float.seed = " << r_float.seed() << '\n';
+
+    // set the seed of the generator
+    r_float.reseed();
+    std::cout << r_float.next() << '\n';
+
+    // generates a random boolean that will be true 25% of the time
+    shard::random<bool> r_bool(0.25);
+    r_bool.reseed();
+    std::map<bool, int> histogram;
+    for (int n = 0; n < 10000; ++n) {
+        ++histogram[r_bool.next()];
+    }
+    for (auto p : histogram) {
+        std::cout << p.first << ' ' << std::string(p.second / 500, '*') << '\n';
+    }
+
+    // generates a random number using a faster method than the above
+    std::cout << shard::fast_random(0, 100) << '\n';
+
+    // set the seed used by 'fast_random'
+    shard::fast_random_seed(std::time(nullptr));
+    std::cout << shard::fast_random(0, 10) << '\n';
+
+    return 0;
+}
---
+++ modules/random/include/shard/random.hpp
@@ -0,0 +1,84 @@
+// Copyright (c) 2021 Miklos Molnar. All rights reserved.
+
+#ifndef SHARD_RANDOM_HPP
+#define SHARD_RANDOM_HPP
+
+#include <shard/meta/type_traits.hpp>
+
+#include <cstdlib>
+#include <random>
+
+namespace shard {
+
+template <typename Distribution>
+class random_base {
+public:
+    using engine_type = std::mt19937;
+    using seed_type = engine_type::result_type;
+    using dist_type = Distribution;
+    using value_type = typename dist_type::result_type;
+    using dist_param_type = typename dist_type::param_type;
+
+public:
+    template <typename... Args>
+    explicit random_base(Args&&... args) : m_seed(engine_type::default_seed), m_dist(std::forward<Args>(args)...) {}
+
+    value_type next() {
+        return m_dist(m_engine);
+    }
+
+    dist_param_type dist_param() const {
+        return m_dist.param();
+    }
+
+    void reseed() {
+        std::random_device rd;
+        reseed(rd());
+    }
+
+    void reseed(seed_type seed) {
+        m_seed = seed;
+        m_engine.seed(seed);
+    }
+
+    seed_type seed() const {
+        return m_seed;
+    }
+
+private:
+    engine_type m_engine;
+    seed_type m_seed {};
+    dist_type m_dist;
+};
+
+// clang-format off
+template <typename T>
+using random =
+std::conditional_t<meta::is_integer<T>::value,
+    random_base<std::uniform_int_distribution<T>>,
+std::conditional_t<std::is_floating_point<T>::value,
+    random_base<std::uniform_real_distribution<T>>,
+std::conditional_t<meta::is_bool<T>::value,
+    random_base<std::bernoulli_distribution>,
+void
+>
+>
+>;
+// clang-format on
+
+/// Seed the fast random number generator
+inline void fast_random_seed(unsigned int seed) {
+    std::srand(seed);
+}
+
+/// Fast random number generator
+///
+/// \details Using the C function 'rand()' is much faster, but has limited
+/// randomness compared to the C++ alternative
+inline int fast_random(int lower, int upper) {
+    return (std::rand() % (upper - lower + 1)) + lower; /* NOLINT */
+}
+
+} // namespace shard
+
+#endif // SHARD_RANDOM_HPP
