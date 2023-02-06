3bcadd8b4f507767526aa30a2da82d8268837658
Miklos Molnar <miklos.molnar@me.com>
2023-02-06 22:55:26 +0100

    Simplify hash combiner

--- modules/utility/include/shard/utility/hash_value.hpp
+++ modules/utility/include/shard/utility/hash_value.hpp
@@ -9,17 +9,14 @@
 namespace shard {
 namespace utility {
 
-template <typename T>
-void hash_combine(std::size_t& seed, const T& value) {
-    seed ^= std::hash<T> {}(value) + 0x9e3779b9 + (seed << 6u) + (seed >> 2u);
-}
-
+/// Append the hash of multiple values to the seed
 template <typename T, typename... Args>
 void hash_combine(std::size_t& seed, const T& value, const Args&... args) {
-    hash_combine(seed, value);
-    hash_combine(seed, args...);
+    seed ^= std::hash<T> {}(value) + 0x9e3779b9 + (seed << 6u) + (seed >> 2u);
+    (hash_combine(seed, args), ...);
 }
 
+/// Get the combined hash of multiple values
 template <typename... Args>
 std::size_t hash_value(const Args&... args) {
     std::size_t seed = 0;
--- tests/utility_test.cpp
+++ tests/utility_test.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2023 Miklos Molnar. All rights reserved.
 
 #include <shard/utility/defer.hpp>
+#include <shard/utility/hash_value.hpp>
 
 #include <doctest.h>
 
@@ -11,5 +12,15 @@ TEST_CASE("utility") {
             auto _ = shard::defer([&] { called = true; });
         }
         REQUIRE(called);
+    }
+
+    SUBCASE("hashing") {
+        auto i1 = shard::hash_value(1, 2, 3);
+        auto i2 = shard::hash_value(1, 2, 3);
+        REQUIRE(i1 == i2);
+
+        auto str1 = shard::hash_value(std::string("foo"));
+        auto str2 = shard::hash_value(std::string("FOO"));
+        REQUIRE(str1 != str2);
     }
 }
