a96739e4a2e81225a29d5f8eb74e41fb26cb7abe
Miklos Molnar <miklos.molnar@me.com>
2023-02-06 22:45:04 +0100

    Update CMake framework helper macro

--- cmake/macos.cmake
+++ cmake/macos.cmake
@@ -2,16 +2,23 @@
 # shard :: cmake :: macos.cmake
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# link the macOS frameworks to the target
+# collect Apple frameworks into the variable
 #
-# usage: shard_link_mac_frameworks(<target> <framework> [...])
-macro(shard_link_mac_frameworks TARGET)
+# usage: shard_find_frameworks(<variable> <framework> [...])
+macro (shard_find_frameworks OUT_VAR)
     foreach (FRAMEWORK_NAME ${ARGN})
         set(FRAMEWORK_LIB ${FRAMEWORK_NAME}_LIB)
         find_library(${FRAMEWORK_LIB} ${FRAMEWORK_NAME})
         if (${FRAMEWORK_LIB})
-            list(APPEND FRAMEWORKS ${${FRAMEWORK_LIB}})
+            list(APPEND ${OUT_VAR} ${${FRAMEWORK_LIB}})
         endif ()
     endforeach ()
+endmacro ()
+
+# link the frameworks to the target
+#
+# usage: shard_link_frameworks(<target> <framework> [...])
+macro (shard_link_frameworks TARGET)
+    shard_find_frameworks(FRAMEWORKS ${ARGN})
     target_link_libraries(${TARGET} PUBLIC ${FRAMEWORKS})
-endmacro()
+endmacro ()
