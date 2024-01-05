7d81c6b51984f89bb187de53ad167b549c0dd1a0
Miklos Molnar <miklos.molnar@me.com>
2024-01-05 15:41:21 +0100

    Automatically detect modules from CMake

--- cmake/utility.cmake
+++ cmake/utility.cmake
@@ -3,7 +3,7 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # enable warnings for the given target
-function(shard_target_enable_warnings TARGET)
+function (shard_target_enable_warnings TARGET)
     if (MSVC)
         target_compile_options(${TARGET} PRIVATE /W4)
     else ()
@@ -15,4 +15,15 @@ function(shard_target_enable_warnings TARGET)
                                -Wno-gnu-zero-variadic-macro-arguments
                                )
     endif ()
-endfunction()
+endfunction ()
+
+# get the subdirectories of the given directory
+function (shard_list_subdirs OUT_DIRECTORIES DIRECTORY)
+    file(GLOB CHILDREN RELATIVE ${DIRECTORY} ${DIRECTORY}/*)
+    foreach (CHILD ${CHILDREN})
+        if (IS_DIRECTORY ${DIRECTORY}/${CHILD})
+            list(APPEND DIRECTORY_LIST ${CHILD})
+        endif ()
+    endforeach ()
+    set(${OUT_DIRECTORIES} ${DIRECTORY_LIST} PARENT_SCOPE)
+endfunction ()
