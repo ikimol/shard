cdff91617303d6462cce3fb70cd03e6e77a62192
Miklos Molnar <miklos.molnar@me.com>
2024-01-05 11:24:55 +0100

    Remove `__` from macro name

--- modules/utility/include/shard/utility/preprocessor.hpp
+++ modules/utility/include/shard/utility/preprocessor.hpp
@@ -2,18 +2,18 @@
 
 #pragma once
 
-/// Extract the head of the argument list
+/// Extract the head of the vararg list
 #define SHARD_VA_HEAD(x, ...) x
 
-/// Extract the tail of the argument list
+/// Extract the tail of the vararg list
 #define SHARD_VA_TAIL(x, ...) __VA_ARGS__
 
 /// Denote a variable as unused to avoid compiler warnings
 #define SHARD_UNUSED(x) ((void)(x))
 
 /// Concatenate the two arguments
-#define __SHARD_CONCAT(a, b) a##b
-#define SHARD_CONCAT(a, b) __SHARD_CONCAT(a, b)
+#define SHARD_INTERNAL_CONCAT(a, b) a##b
+#define SHARD_CONCAT(a, b) SHARD_INTERNAL_CONCAT(a, b)
 
 /// Create a locally (current TU) unique identifier
 #define SHARD_UNIQUE_ID(x) SHARD_CONCAT(x, __COUNTER__)
