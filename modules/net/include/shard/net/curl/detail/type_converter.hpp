da5b857c32f938d57ab9b06d2fe58033c2d64d52
Miklos Molnar <miklos.molnar@me.com>
2024-03-03 20:34:59 +0100

    Adjust cURL type conversions

--- modules/net/include/shard/net/curl/detail/type_converter.hpp
+++ modules/net/include/shard/net/curl/detail/type_converter.hpp
@@ -40,7 +40,7 @@ struct type_converter<std::string> {
     using user_type = std::string;
     using curl_type = char*;
 
-    static curl_type to_curl(const user_type& value) { return const_cast<char*>(value.c_str()); }
+    static curl_type to_curl(const user_type& value) { return const_cast<curl_type>(value.c_str()); }
 };
 
 template <>
@@ -56,7 +56,7 @@ struct type_converter<url> {
     using user_type = url;
     using curl_type = char*;
 
-    static curl_type to_curl(const user_type& value) { return const_cast<char*>(value.to_string().c_str()); }
+    static curl_type to_curl(const user_type& value) { return const_cast<curl_type>(value.to_string().c_str()); }
 };
 
 template <>
@@ -64,7 +64,7 @@ struct type_converter<http::version_t> {
     using user_type = http::version_t;
     using curl_type = long;
 
-    static curl_type to_curl(const user_type& value) { return static_cast<long>(value); }
+    static curl_type to_curl(const user_type& value) { return static_cast<curl_type>(value); }
 };
 
 template <>
@@ -72,7 +72,7 @@ struct type_converter<std::optional<std::string>> {
     using user_type = std::optional<std::string>;
     using curl_type = char*;
 
-    static curl_type to_curl(const user_type& value) { return value ? const_cast<char*>(value->c_str()) : nullptr; }
+    static curl_type to_curl(const user_type& value) { return value ? const_cast<curl_type>(value->c_str()) : nullptr; }
 };
 
 // convenience typedefs
