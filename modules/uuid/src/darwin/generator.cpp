b5226837e5c4fb5813463f2c8309c2f816f0f411
Miklos Molnar <miklos.molnar@me.com>
2025-08-27 12:15:12 +0200

    Update `uuid` generator formatting

--- modules/uuid/src/darwin/generator.cpp
+++ modules/uuid/src/darwin/generator.cpp
@@ -11,22 +11,24 @@ uuid uuid::make_system_uuid() {
     auto id_bytes = CFUUIDGetUUIDBytes(id);
     CFRelease(id);
 
-    std::array<std::uint8_t, 16> bytes = {{id_bytes.byte0,
-                                           id_bytes.byte1,
-                                           id_bytes.byte2,
-                                           id_bytes.byte3,
-                                           id_bytes.byte4,
-                                           id_bytes.byte5,
-                                           id_bytes.byte6,
-                                           id_bytes.byte7,
-                                           id_bytes.byte8,
-                                           id_bytes.byte9,
-                                           id_bytes.byte10,
-                                           id_bytes.byte11,
-                                           id_bytes.byte12,
-                                           id_bytes.byte13,
-                                           id_bytes.byte14,
-                                           id_bytes.byte15}};
+    std::array<std::uint8_t, 16> bytes = {{
+        id_bytes.byte0,
+        id_bytes.byte1,
+        id_bytes.byte2,
+        id_bytes.byte3,
+        id_bytes.byte4,
+        id_bytes.byte5,
+        id_bytes.byte6,
+        id_bytes.byte7,
+        id_bytes.byte8,
+        id_bytes.byte9,
+        id_bytes.byte10,
+        id_bytes.byte11,
+        id_bytes.byte12,
+        id_bytes.byte13,
+        id_bytes.byte14,
+        id_bytes.byte15,
+    }};
 
     return uuid {std::begin(bytes), std::end(bytes)};
 }
